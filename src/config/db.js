622f3883a02714a96d3a70bf8ed6720f9ac9aca2
narasimha vadla <narasimhavadla12@gmail.com>
2026-02-14 23:50:47 +0530

    modal are added

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(
   "ngo",          
   "root",         
-  "12345", 
+  "20F61A0495@l", 
   {
     host: "localhost",
     dialect: "mysql",
